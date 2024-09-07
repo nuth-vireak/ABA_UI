2c5baa1dc74dbab982ff1aec76a17f2f62816566
nuth-vireak <timberr.4t@gmail.com>
2024-09-07 13:44:54 +0700

    Initial Commit

---
+++ ABA_UI/ABA_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABA_UIApp.swift
+//  ABA_UI
+//
+//  Created by KAK-REAK on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ABA_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ABA_UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ABA_UI
+//
+//  Created by KAK-REAK on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
